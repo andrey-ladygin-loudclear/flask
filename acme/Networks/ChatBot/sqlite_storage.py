0f98ec7eb0a88f2f1b65261bc3e1a48ffd6ff7da
Srivoknovski <test@gmail.com>
2018-03-14 22:16:12 +0200

    some small fixes

--- acme/Networks/ChatBot/sqlite_storage.py
+++ acme/Networks/ChatBot/sqlite_storage.py
@@ -8,6 +8,7 @@ class SQLiteStorage():
     transactions = []
 
     def __init__(self, name, db_folder='db'):
+        print('INIT', name, db_folder)
         self.file = '{}/{}.db'.format(db_folder, name)
         self.conn = sqlite3.connect(self.file)
         self.c = self.conn.cursor()
@@ -66,6 +67,7 @@ class SQLiteStorage():
             pass
 
     def get_batch(self, unix, limit):
+        print('Select from file', self.file)
         return pd.read_sql("SELECT * FROM parent_reply WHERE unix > {} AND parent NOT NULL AND score > 0 ORDER BY unix ASC LIMIT {}".format(
             unix, limit
         ), self.conn)
