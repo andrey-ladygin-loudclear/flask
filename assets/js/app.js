85bc3cc284cf8ee532f470b8169f22b9f195e651
Srivoknovski <test@gmail.com>
2018-03-20 21:59:57 +0200

    vue

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 require('./bootstrap');
 window.Vue = require('vue');
 
+require('./components');
 require('./face_net');
 require('./profile');
 require('./login');
--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,32 +1,32 @@
 import Vue from 'vue';
 
 let Webcam = require('./webcam.min');
-//let Vue = require('vue');
 
-Vue.component('mycounter', {
-    //template: `<h1>Hi </h1>`
-    template: '#counter-template',
-    props: ['subject'],
-    data: function() {
-        return { count: 0 }
-    }
-});
-
-let profile_images = Vue.extend({
-    template: `<div><h2>Profile images</h2><div class="loader" v-if="loading">
-       <img class='preloader' src='/static/img/preloader.gif' width='80'/></div>{{ text }}</div>`,
-    props: {
-        loading: false,
-        text: false
-    },
-    watch: {
-        isLoading: function(is_load) {
-            this.loading = !!is_load;
-        }
-    }
-});
-
-Vue.component('profile-images', profile_images);
+// let profile_images = Vue.extend({
+//     template: `<div v-on:profileImage="handleProfileImage"><h2>Profile images</h2>
+//             <div v-for="path in images">
+//                 <img :src="path" class="img-thumbnail"  width="100" />
+//             </div>
+//             <div class="loader" v-if="loading">
+//                 <img class='preloader' src='/static/img/preloader.gif' width='80'/>
+//             </div>
+//         </div>`,
+//     props: {
+//         loading: false,
+//         text: false,
+//         images: []
+//     },
+//     methods: {
+//         handleProfileImage: function(path) {
+//             this.images.push(path)
+//         },
+//         isLoading: function(is_load) {
+//             this.loading = !!is_load;
+//         }
+//     }
+// });
+//
+// Vue.component('profile-images', profile_images);
 
 new Vue({
     el: '#webcam',
@@ -39,8 +39,6 @@ new Vue({
                 <profile-images v-bind:loading="image_processing"></profile-images>
             </div>`,
     data: {
-        points: 500,
-        count: 0,
         image_processing: false,
         message: ''
     },
@@ -61,20 +59,39 @@ new Vue({
         },
         take_snapshot: function (e) {
             this.image_processing = true;
-
-            setTimeout(this.upload_image, 1000);
-            // Webcam.snap( function(data_uri) {
-            //     $('.profile-images').append("<img class='preloader' src='/static/img/preloader.gif' width='80'/>");
-            //     $('.alert').remove();
-            //     uploadFile(data_uri);
-            // } );
+            Webcam.snap(this.upload_image);
         },
-        upload_image: function () {
-            this.image_processing = false;
+        upload_image: function (data_uri) {
+            let _this = this;
+
+            let formData = new FormData();
+            formData.append("fileToUpload", data_uri);
+
+            $.ajax({
+                url: "/upload_profile_image",
+                type: "POST",
+                dataType: 'json',
+                data: formData,
+                processData: false,
+                contentType: false,
+                success: function(response) {
+                    _this.image_processing = false;
+
+                    if (response.success) {
+                        _this.message = $('<div class="alert alert-success">Photo Added!</div>');
+                        _this.$emit('profileImage', { path: response.path });
+                        //$('.profile-images').append(`<img src="${response.path}" class="img-thumbnail"  width="100" />`)
+                    } else {
+                        _this.message = $(`<div class="alert alert-danger">${response.message}</div>`);
+                        //_this.message = $('<div class="alert alert-danger">Please, put your face straight at the webcam!</div>');
+                    }
+                },
+                error: function(jqXHR, textStatus, errorMessage) {
+                    _this.image_processing = false;
+                    console.error(errorMessage);
+                }
+            });
         }
-    },
-    components: {
-        profile_images
     }
 });
 
@@ -100,54 +117,3 @@ Vue.component('my-webcam', {
         return {}
     }
 });
-
-if ($('.profile_____').length) {
-    Webcam.set({
-        width: 320,
-        height: 240,
-        image_format: 'jpeg',
-        jpeg_quality: 90
-    });
-    Webcam.attach( '#my_camera' );
-
-    $('#take_snapshot').click(take_snapshot);
-
-    function take_snapshot () {
-        Webcam.snap( function(data_uri) {
-            $('.profile-images').append("<img class='preloader' src='/static/img/preloader.gif' width='80'/>");
-            $('.alert').remove();
-            uploadFile(data_uri);
-        } );
-    };
-
-    function uploadFile (data_uri) {
-        let formData = new FormData();
-        formData.append("fileToUpload", data_uri);
-
-        $.ajax({
-            url: "/upload_profile_image",
-            type: "POST",
-            dataType: 'json',
-            data: formData,
-            processData: false,
-            contentType: false,
-            success: function(response) {
-                let $alert;
-
-                if (response.success) {
-                    $alert = $('<div class="alert alert-success">Photo Added!</div>');
-                    $('.profile-images').append(`<img src="${response.path}" class="img-thumbnail"  width="100" />`)
-                } else {
-                    $alert = $('<div class="alert alert-danger">Please, put your face straight at the webcam!</div>');
-                }
-
-                $('.preloader').remove();
-                $('#my_camera').after($alert);
-            },
-            error: function(jqXHR, textStatus, errorMessage) {
-                $('.preloader').remove();
-                console.error(errorMessage); // Optional
-            }
-        });
-    }
-}
