f715b37e5078ce1288ded275fc45668e02451234
andrey <andrey@gmail.com>
2018-03-22 13:19:46 +0200

    callouts

--- assets/js/face_net.js
+++ assets/js/face_net.js
@@ -1,112 +1,15 @@
 import client from 'socket.io-client';
 import Vue from 'vue';
 
-const preloader = '<img class="preloader" src="/static/img/preloader.gif" width="35">';
-
 const socket = client.connect('http://' + document.domain + ':' + location.port);
 
 socket.on('connect', function() {
     console.info('Socket connected');
 });
 
-$('.face-net .update').click(function() {
-    const type = $(this).data('type');
-    let $this = $(this).parent();
-    let $logs = $('<div class="logs well"></div>');
-
-    console.log('emit update', type);
-    socket.emit('update', {name: type});
-    $this.append($(preloader));
-    $this.append($logs);
-});
-
-const bs = ['landmark', 'lfw', 'output', 'model'];
-
-for (let i in bs) {
-    socket.on(`log-${bs[i]}`, function(data) {
-        $(`.bs-${bs[i]}`).find('.logs').prepend(data.message + "<br/>");
-    });
-    socket.on(`finish-${bs[i]}`, function(data) {
-        let $node = $(`.bs-${bs[i]}`);
-        $node.find('.info').html('');
-
-        if (data.error) {
-            //$node.find('.info').append('`<p><b>Error:</b> ${data.error}</p>`');
-            $node.find('.logs').append(`<b>Error</b>: ${data.error}`)
-        } else {
-            finishUpdate($node);
-            $node.find('.logs').append("Done! <br/>");
-        }
-
-        if (data.size) {
-            $node.find('.info').append('`<p><b>Size:</b> ${data.size}</p>`');
-        }
-        if (data.message) {
-            $node.find('.info').append('`<p class="alert alert-info"><b>Info:</b> ${data.message}</p>`');
-        }
-    });
-}
-
-let finishUpdate = function($node) {
-    $node.removeClass('bs-callout-danger')
-        .addClass('bs-callout-success')
-        .find('.preloader')
-        .remove();
-};
-
 socket.on('disconnect', function() {
     console.info('Socket disconnected');
 });
-
-// let template = `<div class="bs-callout bs-landmark {% if net.landmarks %}bs-callout-success{% else %}bs-callout-danger{% endif %}">
-//             <h4>{{ name }}</h4>
-//             <p>path: {{ net.landmarks_path }}</p>
-//             <div class="info">
-//                 {{ info }}
-//                 {% if net.landmarks %}
-//                     <p><b>Size</b>: {{ net.landmarks }}</p>
-//                 {% else %}
-//                     <p>No File</p>
-//                 {% endif %}
-//             </div>
-//             <button class="btn btn-info update" data-type="landmarks">Download</button>
-//         </div>`;
-// let Vue = require('vue');
-// Vue.component('callout', {
-//     template: template,
-//     //template: '#counter-template',
-//     props: ['subject'],
-//     data: function() {
-//         return {
-//             name: 'Weights',
-//             info: `<p><b>Size</b>: ${size}</p>`,
-//             count: 0
-//         }
-//     }
-// });
-
-// Vue.component('callout', new Vue({
-//     //el: '#callout',
-//     el: 'callout',
-//     //template: '#callout-template',
-//     delimiters: ['{(', ')}'],
-//     data: {
-//         loading: false,
-//         status: false,
-//         info: 'default',
-//         message: 'adawdaw',
-//     },
-//     // created: function () {
-//     //     console.info('mounted', this.info)
-//     // },
-//     methods: {
-//         update: function() {
-//             console.log('Update', this.info);
-//             this.info += ' 1'
-//         }
-//     }
-// }));
-
 
 Vue.component('callout', {
     template: '#callout-template',
@@ -115,24 +18,56 @@ Vue.component('callout', {
     data() {
         //console.log('this.props.collout.type', this.props.collout.type);
         return {
+            success: false,
             loading: false,
-            info: 'default',
-            counter: 1,
+            showLogs: false,
+            info: '',
             logs: ''
         }
     },
-    // created: function() {
-    //     console.log('mounted mounted mounted');
-    //     const type = this.props.collout.type;
-    //     socket.on(`log-${type}`, (data) => {
-    //         this.logs += data.message + "<br/>";
-    //     });
-    // },
+    created: function() {
+        const type = this._props.callout.type;
+        socket.on(`log-${type}`, this.log);
+        socket.on(`finish-${type}`, this.finish);
+        socket.emit('check', {name: type});
+    },
     methods: {
         update: function() {
-            const type = this.props.collout.type;
+            const type = this.callout.type;
             socket.emit('update', {name: type});
+            this.showLogs = true;
             this.loading = true;
+            this.logs = '';
+            this.info = '';
+        },
+        log: function(data) {
+            if (data.message)
+                this.logs += data.message + "<br />";
+
+            if (data.image) {
+                let arrayBufferView = new Uint8Array( data.image );
+                let blob = new Blob( [ arrayBufferView ], { type: "image/jpeg" } );
+                let urlCreator = window.URL || window.webkitURL;
+                let imageUrl = urlCreator.createObjectURL( blob );
+                this.logs += `<img src="${imageUrl}" width="50" /><br />`;
+            }
+        },
+        finish: function(data) {
+            console.log('Finish', this.callout.type, data);
+            this.loading = false;
+
+            if(data.error) {
+                this.success = false;
+
+                this.info += `<p><b style="color:#860000;">Error:</b> ${data.error}</p>`
+            } else {
+                this.success = true;
+                //this.showLogs = false;
+
+                for (let key in data) {
+                    this.info += `<p><b>${key.capitalize()}:</b> ${data[key]}</p>`
+                }
+            }
         }
     }
 });
@@ -149,3 +84,7 @@ new Vue({
         ]
     }
 });
+
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+};
--- routes/face_net_dashboard.py
+++ routes/face_net_dashboard.py
@@ -1,3 +1,4 @@
+import base64
 import bz2
 import os
 import tarfile
@@ -7,6 +8,7 @@ import numpy as np
 from flask import render_template
 
 from acme.Networks.FaceNet.download_and_extract_model import download_and_extract_model
+from acme.Networks.FaceNet.preprocess import preprocess
 from acme.Networks.FaceNet.test_classifier import get_emmbedings
 from acme.auth import Auth, is_logged_in
 from acme.url import absolute
@@ -16,26 +18,28 @@ from app import app, face_net_instance, socketio_app
 @app.route('/face_net_dashboard', methods=['GET', 'POST'])
 @is_logged_in
 def face_net_dashboard():
-    return render_template('admin/face_net_dashboard.html', user=Auth.user(), net=get_face_net_info())
+    return render_template('admin/face_net_dashboard.html', user=Auth.user())
 
 
-def get_face_net_info():
-    net = {}
-    net['landmarks_path'] = absolute(app.config['FACE_NET_LANDMARKS_FILE'])
-    net['model_path'] = absolute(app.config['FACE_NET_WEIGHTS_DIR'])
-    net['lfw_path'] = absolute(app.config['FACE_NET_DATA_DIR'])
-    net['output_path'] = absolute(app.config['FACE_NET_OUTPUT_DIR'])
-    net['landmarks'] = False
-    net['model'] = False
-    net['lfw'] = False
-    net['output'] = False
+@socketio_app.on('check')
+def check_request(message):
+    name = message['name']
 
-    if os.path.isfile(net['landmarks_path']): net['landmarks'] = sizeof_fmt(os.path.getsize(net['landmarks_path']))
-    if os.path.isdir(net['lfw_path']): net['lfw'] = sizeof_fmt(get_size(net['lfw_path']))
-    if os.path.isdir(net['output_path']): net['output'] = sizeof_fmt(get_size(net['output_path']))
-    if os.path.isdir(net['model_path']): net['model'] = sizeof_fmt(get_size(net['model_path']))
+    try:
+        info = None
 
-    return net
+        if name == 'landmarks': info = get_landmark_info()
+        if name == 'model': info = get_model_info()
+        if name == 'lfw': info = get_lfw_info()
+        if name == 'output': info = get_output_info()
+        #if name == 'prediction': return
+
+        if info:
+            socketio_app.emit('finish-' + str(name), info)
+
+    except Exception as e:
+        socketio_app.emit('finish-' + str(name), {'error': "{}: {}".format(type(e).__name__, str(e))})
+        raise e
 
 
 @socketio_app.on('update')
@@ -47,7 +51,7 @@ def update_request(message):
         if name == 'landmarks': update_landmark()
         if name == 'model': update_model()
         if name == 'lfw': update_lfw()
-        if name == 'output': update_callout()
+        if name == 'output': update_output()
         if name == 'prediction': make_tests()
     except Exception as e:
         socketio_app.emit('finish-' + str(name), {'error': "{}: {}".format(type(e).__name__, str(e))})
@@ -58,8 +62,18 @@ def update_model():
     socketio_app.emit('log-model', {'message': 'Start updating...'})
     download_and_extract_model('20170511-185253', absolute(app.config['FACE_NET_WEIGHTS_DIR']))
     socketio_app.emit('log-model', {'message': 'Done'})
-    size = sizeof_fmt(get_size(absolute(app.config['FACE_NET_WEIGHTS_DIR'])))
-    socketio_app.emit('finish-model', {'size': size})
+    socketio_app.emit('finish-model', get_model_info())
+
+
+def get_model_info():
+    dir = absolute(app.config['FACE_NET_WEIGHTS_DIR'])
+
+    if not os.path.isdir(dir): return {'error': 'Folder does not exists'}
+
+    return {
+        'path': dir,
+        'size': sizeof_fmt(get_size(dir))
+    }
 
 
 def update_landmark():
@@ -76,8 +90,18 @@ def update_landmark():
 
     os.remove(archive)
     socketio_app.emit('log-landmark', {'message': 'Done'})
-    size = sizeof_fmt(os.path.getsize(file))
-    socketio_app.emit('finish-landmark', {'size': size})
+    socketio_app.emit('finish-landmark', get_landmark_info())
+
+
+def get_landmark_info():
+    file = absolute(app.config['FACE_NET_LANDMARKS_FILE'])
+
+    if not os.path.isfile(file): return {'error': 'File does not exists'}
+
+    return {
+        'path': file,
+        'size': sizeof_fmt(os.path.getsize(file))
+    }
 
 
 def update_lfw():
@@ -92,41 +116,71 @@ def update_lfw():
     tar.close()
     os.remove(archive)
     socketio_app.emit('log-lfw', {'message': 'Done'})
-    size = sizeof_fmt(get_size(data))
-    socketio_app.emit('finish-lfw', {'size': size})
+    socketio_app.emit('finish-lfw', get_lfw_info())
 
 
-def update_callout():
-    socketio_app.emit('log-callout', {'message': 'Start updating...'})
-    output_dir = absolute(app.config['FACE_NET_OUTPUT'])
-    preprocess(absolute(app.config['FACE_NET_DATA']), output_dir, 180)
-    socketio_app.emit('log-callout', {'message': 'Done'})
-    size = sizeof_fmt(get_size(output_dir))
-    socketio_app.emit('finish-lfw', {'size': size})
+def get_lfw_info():
+    data = absolute(app.config['FACE_NET_DATA_DIR'])
+    return {
+        'path': data,
+        'size': sizeof_fmt(get_size(data))
+    }
+
+
+def update_output():
+    socketio_app.emit('log-output', {'message': 'Start updating...'})
+    output_dir = absolute(app.config['FACE_NET_OUTPUT_DIR'])
+    preprocess(absolute(app.config['FACE_NET_DATA_DIR']), output_dir, 180)
+    socketio_app.emit('log-output', {'message': 'Done'})
+    socketio_app.emit('finish-output', get_output_info())
+
+
+def get_output_info():
+    dir = absolute(app.config['FACE_NET_OUTPUT_DIR'])
+    size = get_size(dir)
+
+    if size == 0:
+        return {'error': 'Images are not preprocessed'}
+
+    return {
+        'path': dir,
+        'size': sizeof_fmt(size)
+    }
 
 
 def make_tests():
-    print('make_tests')
     crop_dim = 180
+    print('make_tests')
+    socketio_app.emit('log-prediction', {'message': 'Start making tests...'})
     im1 = '/home/srivoknovski/Python/flask/acme/Networks/FaceNet/data/lfw/Aaron_Peirsol/Aaron_Peirsol_0002.jpg'
     im2 = '/home/srivoknovski/Python/flask/acme/Networks/FaceNet/data/lfw/Aaron_Peirsol/Aaron_Peirsol_0004.jpg'
     im3 = '/home/srivoknovski/Python/flask/acme/Networks/FaceNet/data/lfw/Aaron_Tippin/Aaron_Tippin_0001.jpg'
 
+    with open(im1, "rb") as image_file: socketio_app.emit('log-prediction', {'image': (image_file.read())})
+    with open(im2, "rb") as image_file: socketio_app.emit('log-prediction', {'image': (image_file.read())})
+    with open(im3, "rb") as image_file: socketio_app.emit('log-prediction', {'image': (image_file.read())})
+
+    socketio_app.emit('log-prediction', {'message': 'preprocessing test images..., Crop dimension {}'.format(crop_dim)})
     images = []
     images.append(face_net_instance.process_image(im1, crop_dim))
     images.append(face_net_instance.process_image(im2, crop_dim))
     images.append(face_net_instance.process_image(im3, crop_dim))
 
+    socketio_app.emit('log-prediction', {'message': 'loading model...'})
     model_path = absolute(app.config['FACE_NET_WEIGHTS_FILE'])
     embs = get_emmbedings(images=images, model_path=model_path)
+    socketio_app.emit('log-prediction', {'message': 'Model path {} {}'.format(model_path, os.path.getsize(model_path))})
 
     diff1 = np.linalg.norm(embs[0] - embs[1])
     diff2 = np.linalg.norm(embs[0] - embs[2])
 
     print(im1, im2, np.linalg.norm(embs[0] - embs[1]))
     print(im1, im3, np.linalg.norm(embs[0] - embs[2]))
-    message = 'The same persons: {}, The different persons: {}'.format(diff1, diff2)
-    socketio_app.emit('finish-lfw', {'message': message})
+    socketio_app.emit('log-prediction', {'message': 'Done'})
+    socketio_app.emit('finish-prediction', {
+        'The same persons': str(diff1),
+        'The different persons': str(diff2)
+    })
 
 
 def get_size(start_path = '.'):
